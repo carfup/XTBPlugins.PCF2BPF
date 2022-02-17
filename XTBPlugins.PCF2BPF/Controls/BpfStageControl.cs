a88c479e60b5ab08e170a240c30a977867c2f6f0
MscrmTools <tanguy92@hotmail.com>
2022-02-17 17:31:36 +0100

    Added warning display when stage title is not available

--- XTBPlugins.PCF2BPF/Controls/BpfStageControl.cs
+++ XTBPlugins.PCF2BPF/Controls/BpfStageControl.cs
@@ -13,6 +13,20 @@ namespace Carfup.XTBPlugins.Controls
             BackColor = Color.FromArgb(232, 62, 15);
 
             lblStage.Text = stageName;
+
+            if (stageName.StartsWith("Stage"))
+            {
+                pbWarning.Visible = true;
+
+                var descToolTip = new ToolTip()
+                {
+                    ToolTipIcon = ToolTipIcon.Warning,
+                    IsBalloon = true,
+                    ShowAlways = true,
+                    ToolTipTitle = "Why do I see this?"
+                };
+                descToolTip.SetToolTip(pbWarning, "Your BPF definition does not contain title for this stage. Consider updating the PBF definition to force title display");
+            }
         }
     }
 }
